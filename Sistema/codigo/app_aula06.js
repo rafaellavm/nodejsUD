48d12db9c96ac0ae6d3760d705a8582edf0c50b9
rafaelavm <rafaela.vm@gmail.com>
2016-11-18 11:18:51 -0200

    Aula 06: criacao de uma variável com atributos e exibicao na tela

---
+++ Sistema/codigo/app_aula06.js
@@ -0,0 +1,7 @@
+//passando parâmetros para uma variável
+var pessoa = {"nome": "Rafaela", "sobrenome": "Marraschi", "idade": "37"};
+
+//mostrando as informações no prompt:
+console.log(pessoa);
+
+
