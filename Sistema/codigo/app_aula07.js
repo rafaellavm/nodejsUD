18d96c5302021d41c9c072cbdce498784160e853
Rafaela <rafaela.vm@gmail.com>
2016-11-22 01:01:17 -0200

    Aula 07: funções e variáveis

---
+++ Sistema/codigo/app_aula07.js
@@ -0,0 +1,15 @@
+/*function somar(a,b){
+
+    return a+b;
+}
+
+console.log(somar(10,8));*/
+
+var cursos = function(){
+
+    console.log("Em nosso site você encontra mais de 100 cursos");
+
+}
+
+cursos();
+setTimeout(cursos(), 5000);
